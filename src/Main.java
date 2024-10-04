8a05fd1786b1aa4865d83fd9572d493d45b55330
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-10-04 09:26:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*Exercici 1*/-1
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bon dia!");
+        double valor;
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.println("Introdueix un valor entre 0 i 10.");
+            valor = scan.nextDouble();
+
+        } while (valor < 0 || valor > 10);
+        System.out.println("Moltes gràcies");
+
+    }
+}
